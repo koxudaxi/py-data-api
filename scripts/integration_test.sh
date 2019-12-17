7ff97d8f4d602594530f10800f5445aeff2b0281
Koudai Aono <koxudaxi@gmail.com>
2019-12-18 03:54:28 +0900

    split test script

--- scripts/integration_test.sh
+++ scripts/integration_test.sh
@@ -2,5 +2,4 @@
 set -e
 
 export AWS_DEFAULT_REGION=us-west-2
-pytest --cov=pydataapi --ignore-glob=tests/integration/**  --cov-report term-missing  tests
 pytest --cov=pydataapi  --cov-report term-missing --cov-append --docker-compose-no-build --use-running-containers --docker-compose=tests/integration/docker-compose.yml tests/integration/
---
+++ scripts/unittest.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+set -e
+
+export AWS_DEFAULT_REGION=us-west-2
+pytest --cov=pydataapi --ignore-glob=tests/integration/**  --cov-report term-missing  tests
